cde19093336b32c6a334002e037d6683c658f256
Aaron Agraz <aaron_agraz@hotmail.com>
2023-01-04 22:43:52 -0600

    Uploaded semi-final version

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,34 +1,103 @@
 let score = document.getElementById('score');
 let totalScore = document.getElementById('totalScore');
-let countdown = document.getElementById('countdown');
 let count = 0;
 let scoreCount = 0;
 let duration = 0;
 let questionGroup = document.querySelectorAll('.question_group');
 let questionAnswerRow = document.querySelectorAll('.question_group .question_answer_row input');
 let userScore = 0;
+let start = document.getElementById("startButton");
+let end = document.getElementById("endScreen");
+let qCont = document.querySelector(".question-container");
+const timerSpan = document.querySelector("#countdown");
+let initials = document.getElementById("initials");
 
 
-questionAnswerRow.forEach(function(questionAnswerRow){
-    questionAnswerRow.addEventListener('click', function(){
+let intervalId;
 
-        setTimeout(function(){
+let secondsLeft = 60;
+
+const setSecondsLeft = function (value) {
+  if (value < 0) {
+    secondsLeft = 0;
+  } else {
+    secondsLeft = value;
+  }
+  timerSpan.textContent = secondsLeft;
+};
+
+const stopTimer = function () {
+  clearInterval(intervalId);
+};
+
+startButton.addEventListener("click", function () {
+  timerSpan.textContent = secondsLeft;
+  intervalId = setInterval(function () {
+    setSecondsLeft(secondsLeft - 1);
+    if (secondsLeft === 0) {
+      stopTimer();
+      //alert("Ran out of time");
+    }
+  }, 1000);
+ 
+});
+
+
+startButton.onclick = function () {
+    let div = document.getElementById('startScreen');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+
+    }
+    qCont.hidden = false;
+};
+
+questionAnswerRow.forEach(function (questionAnswerRow) {
+    questionAnswerRow.addEventListener('click', function () {
+
+        setTimeout(function () {
             next();
         }, 500)
+        let valid = this.getAttribute("valid");
+        if(valid == "valid"){
+        scoreCount += 25;
+        score.innerHTML = scoreCount;
+        totalScore.innerHTML = scoreCount;
+      }
+      else{
+        setSecondsLeft(secondsLeft - 15);
+      }
+
     })
 
 
 });
 
-function next(){
+
+
+
+
+
+function next() {
     count += 1;
-    for(var i = 0; i < questionGroup.length; i++){
-        questionGroup[i].className = 'question_group';
+    if (count == 4) {
+        qCont.style.display = 'none';
+        theEnd();
     }
-    questionGroup[count].className = 'question_group active';
-    if(count == 6){
-        skip.style.display = 'none';
+    else {
+        for (var i = 0; i < questionGroup.length; i++) {
+            questionGroup[i].className = 'question_group';
+        }
+        questionGroup[count].className = 'question_group active';
     }
 }
 
+function theEnd() {
+    end.style.display = 'block';
+    localStorage.setItem("totalScore, initials")
+}
 
+let
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Coding-Quizz
+For this project, a coding quizz was developed. When the user clicks on the
+wrong answer, the label will turn red and 25 seconds are subtracted from the timer, if the user clicks on the correct answer, the score will increase by 25.
+
+![Coding Quizz](./Assets/img/capture.png)
